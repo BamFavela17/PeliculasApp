7ae12bf302bc1655aa7fc0c2ca9ff98c43e1269c
BamFavela17 <favela.abraham17mx@gmail.com>
2025-10-26 00:29:30 -0700

    Se agregaron los recursos nesesarios, tambien configure  colores en tailwincss.config, agregue (tabs) y movies

---
+++ app/(tabs)/perfile.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const perfile = () => {
+  return (
+    <View>
+      <Text>perfile</Text>
+    </View>
+  )
+}
+
+export default perfile
+
+const styles = StyleSheet.create({})
